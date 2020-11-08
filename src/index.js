5383088a7a1a28cbd4bdbc4020d23f53355d0e49
Bouwe K. Westerdijk <bwesterdijk@newnexus.nl>
2020-11-08 23:32:21 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+//import App from "./App";
 import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div>Helaas doen de sommetjes het even niet... :(</div>
   </React.StrictMode>,
   document.getElementById("root")
 );
